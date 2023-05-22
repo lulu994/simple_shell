483f56d282ab6dcebe9f1093ce1fd25aec8044f3
lulu994 <loubna.neddah@gmail.com>
2023-05-22 13:37:14 +0200

    upd

--- shell.c
+++ shell.c
@@ -6,24 +6,58 @@
  * Return: always 0
  */
 
-int main (void)
+int main(void)
 {
-	char *command;
+	char *command = NULL;
 	size_t command_len = 0;
+	ssize_t read;
 
 
 	for (;;)
 	{
-		printf("$ ");
-		fflush(stdout);
-		if (getline(&command, &command_len, stdin) == -1)
-		{
-			printf("Can't read\n");
-			break;
-		}
-		printf("%s", command);
-	}
+		const char *prompt = "$ ";
 
-	free(command);
-	return (0);
+		write(STDOUT_FILENO, prompt, strlen(prompt));
+
+		read = getline(&command, &command_len, stdin);
+
+		if (read == -1)
+		{
+			if (command == NULL)
+			{
+				perror("Error reading command");
+			} else
+			{
+				write(STDOUT_FILENO, "End of file reached\n", 21);
+				break;
+			}
+			if (pid == -1)
+			{
+				perror("Error forking process");
+				continue;
+			} else if (pid == 0)
+			{
+				char *args[] = {command, NULL};
+
+				execve(command, args, environ);
+				write(STDOUT_FILENO, "Command not found: ", 19);
+				write(STDOUT_FILENO, command, strlen(command));
+				write(STDOUT_FILENO, "\n", 1);
+				_exit(1);
+			}
+			else
+			{
+				int status;
+				waitpid(pid, &status, 0);
+				if (status != 0) {
+					write(STDOUT_FILENO, "Command not found: ", 19);
+					write(STDOUT_FILENO, command, strlen(command));
+					write(STDOUT_FILENO, "\n", 1);
+				}
+        }
+    }
+
+    free(command);
+
+    return (0);
 }
