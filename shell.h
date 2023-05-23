9911b5d1f4c6f471ed83a9d9a7a42fd6cc1bbf7c
oumatoulacen <oumatoulacen@gmail.com>
2023-05-23 19:28:57 +0100

    rm

--- shell.h
+++ shell.h
@@ -1,15 +1,1 @@
-#ifndef SHELL_H
-#define SHELL_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
-#define PATH_MAX_LENGTH 1024
-#define PATH_DELIMITER ":"
-
-extern char **environ;
-
-#endif
